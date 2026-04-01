0403b5be8db79da6829d054488b3acd10d411e15
adityayadav176 <ay6598aditya@gmail.com>
2026-04-01 23:54:19 +0530

    Primitive dataTypes

--- class1.cpp
+++ class1.cpp
@@ -5,9 +5,13 @@ int main(){
     int age = 3;
     char ch = 12;
     float PI = 3.14f;
+    bool isSafe = true;
+    double price = 100.99;
     cout << sizeof(age) << endl;
     cout << sizeof(ch) << endl;
     cout << sizeof(PI) << endl;
+    cout << sizeof(isSafe) << endl;
+    cout << sizeof(price) << endl;
     // cout << "my name is Aditya" << endl << "Yadav" << endl;
     return 0;
 }
