e7e946f5be294e239721b6b2d0e6e674a4b12aaa
adityayadav176 <ay6598aditya@gmail.com>
2026-04-01 23:51:05 +0530

    start dsa journey

---
+++ class1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 3;
+    char ch = 12;
+    float PI = 3.14f;
+    cout << sizeof(age) << endl;
+    cout << sizeof(ch) << endl;
+    cout << sizeof(PI) << endl;
+    // cout << "my name is Aditya" << endl << "Yadav" << endl;
+    return 0;
+}
+
