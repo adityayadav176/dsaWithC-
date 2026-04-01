ae8ccb16deeaddc6bad684090765320c1a91aaf6
adityayadav176 <ay6598aditya@gmail.com>
2026-04-02 00:35:10 +0530

    opertor in cpp

--- class1.cpp
+++ class1.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main(){
 
     // primitive dataTypes
+
+    // int, char, float, bool, double
     // int age = 3;
     // char ch = 12;
     // float PI = 3.14f;
@@ -17,13 +19,36 @@ int main(){
     // cout << "my name is Aditya" << endl << "Yadav" << endl;
 
     // type casting
-    char grade = 'A';
-    double price = 100.99;
+    // char grade = 'A';
+    // double price = 100.99;
 
-    int newPrice = (int)price;
-    cout << newPrice << endl;
-    int value = grade;
-    cout << value << endl;
+    // int newPrice = (int)price;
+    // cout << newPrice << endl;
+    // int value = grade;
+    // cout << value << endl;
+
+    // double price;
+    // cout << "Enter your price :";
+    // cin >> price;
+
+    // cout << "your age is : " << price << endl;
+
+    // int a, b;
+    // cout << "enter first number : ";
+    // cin >> a;
+    
+    // cout << "enter secound number : ";
+    // cin >> b;
+    // int sum = a + b;
+    // cout << sum << endl;
+
+    int a = 10;
+
+    // int b = ++a;
+    int b = a--;
+    cout << "b = " << b << endl;
+    cout << "a = " << a << endl;
+
     return 0;
 }
 
