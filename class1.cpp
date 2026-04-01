8e44721fb963d6877903daf255d6ab1275d08155
adityayadav176 <ay6598aditya@gmail.com>
2026-04-02 00:00:27 +0530

    type casting

--- class1.cpp
+++ class1.cpp
@@ -2,17 +2,28 @@
 using namespace std;
 
 int main(){
-    int age = 3;
-    char ch = 12;
-    float PI = 3.14f;
-    bool isSafe = true;
-    double price = 100.99;
-    cout << sizeof(age) << endl;
-    cout << sizeof(ch) << endl;
-    cout << sizeof(PI) << endl;
-    cout << sizeof(isSafe) << endl;
-    cout << sizeof(price) << endl;
+
+    // primitive dataTypes
+    // int age = 3;
+    // char ch = 12;
+    // float PI = 3.14f;
+    // bool isSafe = true;
+    // double price = 100.99;
+    // cout << sizeof(age) << endl;
+    // cout << sizeof(ch) << endl;
+    // cout << sizeof(PI) << endl;
+    // cout << sizeof(isSafe) << endl;
+    // cout << sizeof(price) << endl;
     // cout << "my name is Aditya" << endl << "Yadav" << endl;
+
+    // type casting
+    char grade = 'A';
+    double price = 100.99;
+
+    int newPrice = (int)price;
+    cout << newPrice << endl;
+    int value = grade;
+    cout << value << endl;
     return 0;
 }
 
