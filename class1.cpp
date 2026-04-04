ea2d2a22f8926dafba96bb54f75344a473992bdc
adityayadav176 <ay6598aditya@gmail.com>
2026-04-04 22:18:49 +0530

    changeArray with fuction

--- class1.cpp
+++ class1.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void ChangeArray(int arr[], int size){
+    for(int i = 0; i < size; i++){
+        arr[i] = 2 * arr[i];
+    }
+}
 int main()
 {
+    int arr[] = {1, 2, 3};
+    ChangeArray(arr, 3);
 
-    int arr[] = {12, 34, 50, -2, 1000, 2000 ,12121, 2, 0, 1212, -121212};
-    int size = 11;
-    int maxVal = arr[0];
-    int minVal = arr[0];
-
-    for (int i = 0; i < size; i++){
-        if(arr[i] > maxVal){
-            maxVal = arr[i];
-        }
-        if(arr[i] < minVal){
-            minVal = arr[i];
-        }
-    }
-
-    cout << "minVal : " << minVal << endl;
-    cout << "maxVal : " << maxVal << endl;
-        return 0;
+    cout << "in main\n";
+    for(int i= 0; i <3; i++){
+        cout <<  arr[i] << " ";
+    } 
+    return 0;
 }
