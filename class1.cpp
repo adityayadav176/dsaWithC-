63548d657500a567e458ba9feff661721543c541
adityayadav176 <ay6598aditya@gmail.com>
2026-04-05 23:26:29 +0530

    unique valuse in the array

--- class1.cpp
+++ class1.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int calSum(int arr[], int size){
-    int sum = 0;
-    for(int i = 0; i < size; i++){
-        sum += arr[i];
-    }
-    return sum;
-}
-int main(){
-    int arr[] = {1, 3, 4, 5, 6};
+int main() {
+    int arr[] = {1, 2, 2, 3, 4, 3, 5};
     int size = sizeof(arr) / sizeof(int);
-    
-     cout << calSum(arr, size)<< endl;
-    
+
+    for(int i = 0; i < size; i++) {
+        bool isUnique = true;
+
+        for(int j = i + 1; j < size; j++) {
+            if(arr[i] == arr[j]) {
+                isUnique = false;
+                break;
+            }
+        }
+
+        if(isUnique) {
+            cout << arr[i] << " ";
+        }
+    }
+
     return 0;
 }
