b4aa7a48418de2adc587d5359654f9119a6b34e1
adityayadav176 <ay6598aditya@gmail.com>
2026-04-05 22:33:54 +0530

    calculate sum

--- class1.cpp
+++ class1.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 using namespace std;
 
-
+int calSum(int arr[], int size){
+    int sum = 0;
+    for(int i = 0; i < size; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
 int main(){
     int arr[] = {1, 3, 4, 5, 6};
     int size = sizeof(arr) / sizeof(int);
-    int start = 0;
-    int end = size - 1;
     
-
-    while(start < end){
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-
-        start++;
-        end--;
-    }
-
-    for(int i = 0; i< size; i++){
-        cout << arr[i] << " ";
-    }
+     cout << calSum(arr, size)<< endl;
+    
     return 0;
 }
