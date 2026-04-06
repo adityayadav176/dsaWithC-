dd24f1bf08355b52ef071fb2189c543ea5ab4d86
adityayadav176 <ay6598aditya@gmail.com>
2026-04-06 22:20:10 +0530

    refactor: update max subarray sum calculation and modify input array

--- class1.cpp
+++ class1.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 using namespace std;
 
 int main(){
-    int arr[] = {20, 33, -21, 121, -33};
+    int arr[] = {3, -4, 5, 4, -1, 7, -8};
     int size = sizeof(arr) / sizeof(int);
+    int maxSum = INT_MIN;
     for(int st = 0; st < size; st++){
+        int currSum = 0;
         for(int end = st; end < size; end++){
-            for(int i = st; i <= end; i++){
-                cout << arr[i];
-            }
-            cout << " ";
+            currSum += arr[end];
+            maxSum = max(currSum, maxSum);
         }
-        cout << endl;
     }
+
+     cout << "max subarray sum = " << maxSum << endl;
     return 0;
 }
