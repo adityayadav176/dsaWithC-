46903ce0cdf520e3f2214f24b56fd895c3b621de
adityayadav176 <ay6598aditya@gmail.com>
2026-04-07 22:30:34 +0530

    pair sum bruth force approch

--- class1.cpp
+++ class1.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include <vector>
-// #include <climits>
+#include <climits>
 using namespace std;
 
 vector<int> pairSum(vector<int>  nums, int target){
     vector<int> ans;
     int n = nums.size();
-    int i = 0, j = n - 1;
 
-    while( i < j){
-        int ps = nums[i] + nums[j];
-        if(ps > target){
-            j--;
-        }else if(ps < target){
-            i++;
-        }else{
-            ans.push_back(i);
-            ans.push_back(j);
-            return ans;
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j <n; j++){
+            if(nums[i] + nums[j] == target){
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
         }
     }
     return ans;
