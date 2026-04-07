810e1955740819692d9df9b4dd482fed40c20e28
adityayadav176 <ay6598aditya@gmail.com>
2026-04-07 22:29:39 +0530

    pair sum optimized approce

--- class1.cpp
+++ class1.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <climits>
+// #include <climits>
 using namespace std;
 
-int main(){
-    int arr[] = {3, -4, 5, 4, -1, 7, -8};
-    int size = sizeof(arr) / sizeof(int);
-    int maxSum = INT_MIN;
-    for(int st = 0; st < size; st++){
-        int currSum = 0;
-        for(int end = st; end < size; end++){
-            currSum += arr[end];
-            maxSum = max(currSum, maxSum);
+vector<int> pairSum(vector<int>  nums, int target){
+    vector<int> ans;
+    int n = nums.size();
+    int i = 0, j = n - 1;
+
+    while( i < j){
+        int ps = nums[i] + nums[j];
+        if(ps > target){
+            j--;
+        }else if(ps < target){
+            i++;
+        }else{
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
         }
     }
-
-     cout << "max subarray sum = " << maxSum << endl;
+    return ans;
+}
+int main(){
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> ans = pairSum( nums, target);
+    cout << ans[0] << "," << ans[1] << endl;
     return 0;
 }
